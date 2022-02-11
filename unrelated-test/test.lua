4bbdfd609b92b61351be4a7824b44945b0b7133b
randomstring1126 <pullstrings@protonmail.com>
2022-02-11 22:16:31 +0100

    Added a license

--- unrelated-test/test.lua
+++ unrelated-test/test.lua
@@ -1,29 +1,5 @@
-function split(input)
-	local words = {}
-	for word in input:gmatch("%S+") do
-		if string.sub(word, 1, 1) == '"' then
-			quoted = true
-		end
+local sql = require "sqlite3"
+local db = sql.open("../src/test.db")
 
-		if string.sub(word, #word) == '"' then
-			quoted = false
-			sentence = sentence .. " " .. word:sub(1, -2)
-			table.insert(words, sentence)
-			sentence = nil
-		elseif quoted and sentence then
-			sentence = sentence .. " " .. word
-		elseif quoted and not sentence then
-			sentence = word:sub(2)
-		else
-			table.insert(words, word:lower())
-		end
-	end
-	return words
-end
-
-string = [[hello "e a t" llll]]
-bs = split(string)
-
-for i = 1, #bs do
-	print(bs[i])
+function drawer(name, ...)
 end
